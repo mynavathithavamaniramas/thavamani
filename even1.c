5583703e6bd3a1237571eead1532110b39314c61
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2017-09-02 13:08:03 +0530

    Create even1.c

---
+++ even1.c
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ double a;
+ flot g;
+ 
+ 
+ cout<<"please enter num"<<endl;
+cin>>a;
+cout<<"enter num pleaseeeeeeeeeee program by programology site abdullah moeen"<<endl;
+cin>>g;
+while(a<g){
+ 
+ if(a%2==0)
+ {
+ cout<<"even no. ="<<a<<endl;
+ 
+ }
+ a=a+1;
+}
+}
+Advertisements
