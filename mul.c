bf30202bd558aee76dec33e4daafba57788878e9
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2018-02-09 14:58:38 +0530

    Update mul.c

--- mul.c
+++ mul.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    flot n, i;
+    int n, i;
 
     printf("Enter an integer: ");
     scanf("%d",&n);
 
     for(i=1; i<=10; ++i)
     {
-        printf("%d * %d = %d \n", n, i, n*i);
+       printf("\n%d",n*i);
     }
-    
-    return 0;
 }
